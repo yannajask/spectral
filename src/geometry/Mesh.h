78d14d8e7f981bea82561873e11795b7afba3e57
Yanna <yannajask@gmail.com>
2025-12-15 20:08:15 -0500

    oops clean up mesh header file

--- src/geometry/Mesh.h
+++ src/geometry/Mesh.h
@@ -2,66 +2,16 @@
 
 #include "Object.h"
 #include "Triangle.h"
-#include <fstream>
-#include <sstream>
-#include <iostream>
-#include <charconv>
 
 class Mesh : public Object {
     public:
-        explicit Mesh(const std::string& objPath) {
-            // to do: move this into a utility class or function
-            // also want to expand functionality later
-            std::ifstream objFile(objPath);
-            std::vector<Vec3> vertices;
-            std::string line;
+        explicit Mesh(const std::string& objPath);
 
-            while (std::getline(objFile, line)) {
-                std::stringstream ss(line);
-                std::string op;
-                ss >> op;
+        void addTriangle(const Triangle& triangle);
 
-                if (op == "v") {
-                    Vec3 v;
-                    ss >> v.x >> v.y >> v.z;
-                    vertices.push_back(v);
-                } else if (op == "f") {
-                    std::string v0, v1, v2;
-                    ss >> v0 >> v1 >> v2;
-                    int i = 0, j = 0, k = 0;
+        void addTriangle(const Vec3& a, const Vec3& b, const Vec3&c);
 
-                    std::from_chars(v0.data(), v0.data() + v0.size(), i);
-                    std::from_chars(v1.data(), v1.data() + v1.size(), j);
-                    std::from_chars(v2.data(), v2.data() + v2.size(), k);
-
-                    addTriangle(vertices[i - 1], vertices[j - 1], vertices[k - 1]);
-                }
-            }
-        }
-
-        void addTriangle(const Triangle& triangle) {
-            triangles.push_back(triangle);
-        }
-
-        void addTriangle(const Vec3& a, const Vec3& b, const Vec3&c) {
-            triangles.push_back(Triangle(a, b, c));
-        }
-
-        virtual bool hit(const Ray& ray, float tmin, float tmax, HitRecord& record) const override {
-            HitRecord tmp;
-            bool hit = false;
-            float closest = tmax;
-
-            for (const Triangle& triangle: triangles) {
-                if (triangle.hit(ray, tmin, closest, tmp)) {
-                    hit = true;
-                    closest = tmp.t;
-                    record = tmp;
-                }
-            }
-
-            return hit;
-        }
+        virtual bool hit(const Ray& ray, float tmin, float tmax, HitRecord& record) const override;
 
     private:
         std::vector<Triangle> triangles;
